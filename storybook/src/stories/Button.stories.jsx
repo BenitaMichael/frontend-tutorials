c17a01ab22056d0ed782ab76157bf4cda9cf9797
BenitaMichael <benubiaka@gmail.com>
2024-06-21 17:35:07 +0100

    feat: practicing with adding more stories to Button.stories

--- storybook/src/stories/Button.stories.jsx
+++ storybook/src/stories/Button.stories.jsx
@@ -2,7 +2,8 @@ import Button from '../components/Button'
 
 export default{
     title: "Button",
-    component: Button
+    component: Button,
+    argTypes: {handleClick: {action: "handleClick"}}
 }
 
 const Template = args => <Button {...args} />
@@ -12,4 +13,18 @@ Red.args = {
     backgroundColor: 'red',
     label: 'Press Me',
     size: 'md'
+}
+
+export const Green = Template.bind({})
+Green.args = {
+    backgroundColor: 'green',
+    label: 'Press Me',
+    size: 'md'
+}
+
+export const Large = Template.bind({})
+Large.args = {
+    backgroundColor: 'green',
+    label: 'Press Me',
+    size: 'lg'
 }
