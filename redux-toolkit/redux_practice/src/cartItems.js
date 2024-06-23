e15d653e81948e0552cb43b0eece8a08b2ee2fa4
BenitaMichael <benubiaka@gmail.com>
2024-06-23 12:32:02 +0100

    feat: starter files

---
+++ redux-toolkit/redux_practice/src/cartItems.js
@@ -0,0 +1,31 @@
+const cartItems = [
+    {
+      id: 'rec1JZlfCIBOPdcT2',
+      title: 'Wedding heels',
+      price: '599.99',
+      img: 'https://images.pexels.com/photos/5317440/pexels-photo-5317440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      amount: 1,
+    },
+    {
+      id: 'recB6qcHPxb62YJ75',
+      title: 'Black heels',
+      price: '399.99',
+      img: 'https://images.pexels.com/photos/10827097/pexels-photo-10827097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      amount: 1,
+    },
+    {
+      id: 'recdRxBsE14Rr2VuJ',
+      title: 'Nude heels',
+      price: '699.99',
+      img: 'https://images.pexels.com/photos/10114295/pexels-photo-10114295.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+      amount: 1,
+    },
+    {
+      id: 'recwTo160XST3PIoW',
+      title: 'Slides',
+      price: '599.99 ',
+      img: 'https://images.pexels.com/photos/12896849/pexels-photo-12896849.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
+      amount: 1,
+    },
+  ];
+  export default cartItems;
